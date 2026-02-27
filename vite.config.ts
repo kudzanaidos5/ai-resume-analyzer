795cc0abbeeb97776dc9b34f69f095a9a6df0048
kudzanaidos5 <kudzanaidos5@gmail.com>
2026-02-27 15:06:36 +0200

    Progress

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    allowedHosts: [
+      'b73c-102-210-115-1.ngrok-free.app' // Add your specific ngrok host here
+    ]
+  }
 });
